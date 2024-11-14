06e6e0b6d4983a57f7e539b6f5c1125d9d8bbf5b
timeipert <tim.eipert@gmail.com>
2024-11-14 16:08:06 +0100

    changed default alignment

--- dist/script.js
+++ dist/script.js
@@ -3,101 +3,52 @@ const { createApp } = Vue;
 createApp({
     data: () => ({
         message: 'Hello Vue!',
-        alignmentText: '> Aa13\n' +
-            '---dcfdfgfghgh-jhhgfhhkhgfegjhgf-d------dffghg----gcd---ddgf\n' +
-            'ghfecdcddddfefggghgfgfhgggjkhhf-fhgggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgfffeedefdfghhhhhhkhhhgghgfghhhhhkhgffed----------\n' +
-            '---\n' +
-            '> Be40078\n' +
-            '---dcfdfgfghgj-hhhgfhhkhgfggjhgf-d------dfefgh----gcd---ddfg\n' +
-            'fghfcd-ddddfefggghgfgfhgggjkhhf-fhgggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgfefeedefdfghhhhhhkhhhgghgfghhhhhkhgfffed---------\n' +
-            '---\n' +
-            '> Bv34\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfeggjhg-fff---dfefghg----cdf-ffddfg\n' +
-            'fghfec--dddd-fefggghggfhgggkkhh-ffhggjhggffghghjgfghghhjgfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n' +
-            '> Gr807\n' +
-            '-ffdcfdfgfghgh-hhgfhh-khgefggjhg-fff---ddfefgh----gcd---ddgf\n' +
-            'ghfecd-ddddfefggghgfgfhgggjkhhf-fhgggjhggffghghkgfghghhkgfff\n' +
-            'fgfddefggffgfefedefdfghhhhhhkhhhgghgfghhhhhkhgfd------------\n' +
-            '---\n' +
-            '> Klo407\n' +
-            '-ffdcfdfgfghgh-hhgfhh-khgefggjhgffffffddfefghg----gcdfffddfg\n' +
-            'fghfec-dddddfefggghgfgfhgggjkhh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfddefggffgfefefdefd---------------------------------------\n' +
-            '---\n' +
-            '> KönD\n' +
-            '---dcfdfgfghghjhhgfhh-jhgfeghjhg-f------dfefgh----gcd---ddgf\n' +
-            'ghfecd-ddddfefggghgfgfhgggjkhhfeghgggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgfefedefd-fghhhhhhkhhhgghgfghhhhhkhgffedhh--khgffe\n' +
-            'd--\n' +
-            '> Mod7\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfggjhgfffff---dfefghg----ddf-ffddfg\n' +
-            'fghfec--ddddfefggghgfgfhgggkkhh-ffhggjhggfffhfhjgfghfhhjgfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n' +
-            '> Mop159\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfeggjhgffff---dfefghg----ddf-ffddfg\n' +
-            'fghfec-ddddd-fefggghggfhgggkkhh-ffhggjhggffghghjgfghghhjgfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n' +
-            '> MüU156\n' +
-            '---dcfdfgfghgh-hhgfghhkhgfgghjhg-f------ddffgh----gcd---fddg\n' +
-            'fghfec-ddddfefggghgfgfhggghjkhh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfeddfggffg-feedefdfghhhhhhkhhhgghgfghhhhhkhgffefdh-hkhgffe\n' +
-            'fd-\n' +
-            '> NY905\n' +
-            '-ffdcfdfgfghgh-hhgfhh-khgfggjhgf-ffd---dfefghg----gcd-fefddg\n' +
-            'fghfec-ddddfefggghgfgfhggghjkhh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgfeffedefdfghhhhhhkhhhgghgfghhhhhkhgfffdhh--khgfff\n' +
-            'd--\n' +
-            '> NauI\n' +
-            '-ffdcfdfgfghgh-hhgfghhkhgfgghjhgffff---ddfefgh----gcd-fffddg\n' +
-            'fghfec-ddddfefggghgfgfhgggh-khh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgfefeedefdfghhhhhhkhhhgghgfghhh-------------------\n' +
-            '---\n' +
-            '> Pa1235\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfeggjhg-fff---fedfefgghgddff-feddfg\n' +
-            'fghfec-dddddfefggghgfgfhgghkkhh-ffhggjhggfffhghjgfghfhhkgfff\n' +
-            'fgfddfggffgfefeedeedfghhhhhhkh-------fghhhhkhgffffed--------\n' +
-            '---\n' +
-            '> Pa776\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfggjhgfffff---dfefggh----gcd-fffddf\n' +
-            'gfghfe---cdddfefggghggfhgggkkhh-ffhghjhggffghghjgfghghhjgfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n' +
-            '> Pa903\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgfggjhgf-fff---dfefghg----ddf-ffddfg\n' +
-            'fghfec--dddd-fefggghggfhgghkkhh-ffhghjhggffghghjgfghghhjgfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n' +
-            '> Pre2\n' +
-            '---dcfdfgfghgh-hhgfhh-khgfeggjhg-f------ddffgh----gcd---ddgf\n' +
-            'ghfecd-ddddfefggghgfgfhgggjkhhh-hhgggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffgffeedefd-----------------------------------------\n' +
-            '---\n' +
-            '> Pro12\n' +
-            'ffdcdfdfgfghgh-hhgfhh-jhgfegjhgf-fff---dfefghg-----dd-fffddf\n' +
-            'eghfec-dddddfefgghgfgfhgghkkhhf-fhgghjhggfffhfhjgfghfhhkgfff\n' +
-            'fgfddfggffgfefeedeedfghhhhhhkhhhgghgfghhhhhkhgffffedhhkhgfff\n' +
-            'fed\n' +
-            '> SCan7\n' +
-            '---dcfdfgfghgh-hhgfghhkhgfgghjhg-f------ddffgh----gcd---fddg\n' +
-            'fghfcd-dddfefggghgfgfhgfgghjkhh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfddefggffgffeedefdfghhhhhhkhhhgghgfghhhhhkhgffefdh-hkhgffe\n' +
-            'fd-\n' +
-            '> Wü165\n' +
-            '---dcfdfgfghgh-hhgfhh-khgfgggjhg-f------ddffgh----gcd---ffdd\n' +
-            'gfghfc-ddddfefggghgfgfhggg-jkhh-ffhggjhggffghghkgfghghhkgfff\n' +
-            'fgfddfggffg--ffedefdfghhhhhhkhhhgghgfghhhhhkhgfd------------\n' +
-            '---\n' +
-            '> Zü106\n' +
-            '-ffdcfdfgfghgh-hhgfhh-jhgffghjhg-fff---dfefghg----ddf-ffddfg\n' +
-            'fghfec-dddddfefggghgfgfhgggkkhh-ffhghjhggffghghjgfghghhjhfff\n' +
-            'fgfddfggffgfefeedeed----------------------------------------\n' +
-            '---\n',
+        alignmentText: '>Aa13\n' +
+            'glllnl---klkmllg-llnlkjhlkkklk-kghgghkjklnmnlkkkklkmlkhkkjhk---hhkk------------------kmlkkklkkjhkhkml-lhkkh-----hkhkjghggkjkllllllmnmmmllmljklllnml-------------------------------kkjhg\n' +
+            '>Be40078\n' +
+            'glllnl---klkmllg-lnlkjhkklkkkl-kghgghk-jkllkkkklkmlkkhkkjhkh---hkkk------------------mlkkkkljkjhkhkml-lhkkk---hhhkhkjghggkjkllllllnmmmllmljklllnml----------------------------------khg\n' +
+            '>Be792\n' +
+            'glllnl---klkmllg-lmlkjh--lkkkl-kghgghk-klnmnlkkkklkmlkkhhkhk---hhk-------------------kkmlkkkklkjkhkml-ghk------hhkjhkghggkjkllllllnmmmllmljkllllmnmlkk------------------------------jhg\n' +
+            '>Ful62\n' +
+            'glllnl---klkmllg-lmlkjhkklkkkl-kghgghk-klnmnlkkkklkmlkkkhkhh-------------------------kkkmlkkkklkkhkml-ghkkj---hhhkhjkghggkjkllllllnmmmllmljklllmnmlk--------------------------------hgh\n' +
+            '>Gr807\n' +
+            'glllml---klkmllg-lmlkjhkklkkkl-kghgghk-klmnlkkkklkmlkjkhkjk----jhkhh---------kjkkm---lkkklklkkjhkhkml-lhkkk---hhhkhjkghggjkllllllmmmmllmljklllnl------------------------------------khg\n' +
+            '>Ka5\n' +
+            'glllnl---klkmllh-lmlkjhkklkkkl-kghgghk-jklnmnlkkkklkmlkkkhkk---kjkhh---------kj------kkmlkkklkkhkhkml--hkkh---hhkjhjkghggkjkllllmmmllmljkllmnmlkkkjkhgkjkl-----------------l-mnmlkkjkhg\n' +
+            '>Klo407\n' +
+            'glllmlkkkklkmllg-lmlkjhkklkkklkkghgghk-klmnlkkkklkmlkjkkkkhk---jkjkhh-----kjkkmlkkkkkkkkklklkkjhkhkml-hkkkk---hhkhjkghgg---------------------------------------------------------------\n' +
+            '>KönD\n' +
+            'glllnl---klkmllg-lnlkjhkklkkklkkghgghk-jkllkkkklkmlkjkkhkkhk---hkkk------------------mlkjkkljkjhkhkml--hkkk---hhhkhkjghggkjkllllllnmmmmllmljklllnmlkkjhg----------------------lnmlkkjhg\n' +
+            '>Mod7\n' +
+            'glllmlkkkklkmllgllmlkjhkklkkklkkghgghk-klmnlkkkklkmlkkkkkhjk---jkjjh---------kjkkml--kkkkkkkljkjjhkml-hkkkk---hhkhjkghgg---------------------------------------------------------------\n' +
+            '>MüU156\n' +
+            'glllml-kjklkmllg-lmlkjhjklkkklkkghgghk-jklnmnlkkkklkmlkkhkj----jhkhh---------kjkkm---lkkkklkkjkhhklml-ghkkk-----khhkhghggkjkllllllnmmmllmljkllllmnmlkkjkhg-----------------llmnmlkkjkhg\n' +
+            '>NY905\n' +
+            'glllnl-kkklkmllg-lmlkjhkklkkklkkghgghk-klnmnlkkkklkmlkkkkhhk---jkkhh---------kjkkml--kkkkkklkkjkkhkml-ghkkj--hhhkjhkkghggkjkllllllnmmmllmljkllllmnmlkkkhgl-----------------l-mnmlkkkhgh\n' +
+            '>NauI\n' +
+            'glllnlkkkklkmllg-lmlkjhkklkkklkkghgghk-klnnnlkkkklkmlkkkkkhhkjkkhkhh---------kjkkmlkkkkkkkkklkkhkhkml-ghkkk--khhhkhhkghggkjkllllllnmmmllmljkllllmnmlkkkkhg-----------------l-lmnmlkkkhg\n' +
+            '>Pa1235\n' +
+            'glllmlkkkklkmllgllmlkjhkklkkklkkghgghk-klnnlkkkkklkmlkjkkkkh---jkjkjhjhkjhkkmlkjkkkkjkkkkhjkjkjhjhkml-hkkkk---khhkhkghgggkjkllllll-----lgkjkllllmn----------------------------mlkkkkjhg\n' +
+            '>Pa776\n' +
+            'glllmljkkklkmllg-lmlkhhkklkkklkk-hgghk-klmnlkkkklkmlkjkkkkhk---jkjjh---------kjkkmljkkkjkkkklkkjjhkml-hjkkk---hhkhjkghgg---------------------------------------------------------------\n' +
+            '>Pa903\n' +
+            'glllmlkkkklkmllg-lmlkjhkklkkklkkghgghk-klmnlkkkklkmlkjkkkkhk---jkjjh---------kjkkmlkkkkkkkkkljkjjhkml-hkkkk---hhkhjkghgg---------------------------------------------------------------\n' +
+            '>Pro12\n' +
+            'glllml-hkklkmllgllmlkjhkklkkklkkghgghk-klmnlkkkklkmlhkkkkhjk---jkjjh---------kjkkml--hkkkkkkljkjjhkml-lhkkkkkhhhkjhkghgggkjkllllllnmmmllmljkllllmnmlk----------------------------kkkjhg\n' +
+            '>Rou250\n' +
+            'glllmlkkkklkmllgllmlkjhkklkkkl-kkhgghk-jlmnlkkkklkmlkjkkkkhj---kjkjjh--------kjkkml-kkkkkkkkljkjjhkml-hkkkk---khhkhkghgggkjkllllllnmmmmllmkkjklllmnmlkkkjhggkjklllllgkjklllllmnmlkkkjhg\n' +
+            '>SCan7\n' +
+            'glllml-kjklkmllg-lmlkjh--lkkkl-kghgghk-klnmnlkkkklkmlkkklkjk---hhkkk---------m-------lkkkkklkjkhhklml-ghkkj---hhhkhjkghggkjkllllllnmmmllmljkllllmnmlkkjkhg-----------------llmnmlkkjkhg\n' +
+            '>Sal20\n' +
+            'glllnl-kjklkmllg-lmlkjh--lkkkl-kghgghk-jklnmnlkkkklkmlkkkhkj---khkhh---------kjkk----mlkkkkklkkhkhklm-lghkk---jhhhkhkghggkjkllllnmmmllmmk----------------------------------------------\n' +
+            '>Si29\n' +
+            'glllml--kklkmllgllmlkjhkklkkklkkghgghk-klnnnlkkkklkmlkkkkkhj---kjjhh---------kjkkmlkkkkkkkkklkkjjhkml-hkkkk---khhhkhkghggklllllllmnmlkkk--------------------------------------------jhg\n' +
+            '>Wo491\n' +
+            'glllnl---klkmllg-lmlkjhkklkkkl-kghgghk-klnmnlkkkklkmlkkjkhhk---kjkhh---------k-------kkmlkkklkkjkhkml-ghkkk---hhhkhhkghggkjkllllllnmmmllmljkllllmnmlkkjhgl-------------------lmnmlkkjhg\n' +
+            '>Wü165\n' +
+            'glllnl---klkmllg-lmlkjh--lkkkl-kghgghk-jklnmnlkkkklkmlkkkhkk---hh--------------------kkkmlkkklkkkhkml-lhkk----hhhkhhkghggkjkllllllnmmmllmljklllmnl----------------------------------khg\n' +
+            '>Zü106\n' +
+            'glllmlkkkklkmllgllmlkjhkklkkklkkghgghk-klmnlkkkklkmlkkkkkhjk---jkjjh---------kjkkml--kkkkkkkljkjjhkmlghkkkk---hhkjhkghgg---------------------------------------------------------------\n',
         solo: null,
         highlight: null,
     }),
